0cbb3b898cad6f6cbb513f58247109f90d6024b7
aananya <aananyanagpal@gmail.com>
2024-10-01 17:54:18 +0530

    backend, gradle setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:emart_app/views/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'consts/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'apikey',
+    appId: 'appId',
+    messagingSenderId: 'messagingSenderId',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+  ));
   runApp(const MyApp());
 }
 
