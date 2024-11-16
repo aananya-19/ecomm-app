6779fd6b554860a75f24fd6737d7f74a889a5349
aananya <aananyanagpal@gmail.com>
2024-11-17 00:02:21 +0530

    profile data displayed

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,5 +1,22 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfileController extends GetxController{
-  
+class ProfileController extends GetxController {
+  var profileImgPath = ''.obs;
+
+  //textfield
+  var nameController = TextEditingController();
+  var passController = TextEditingController();
+  changeImage(context) async {
+    try {
+      final img = await ImagePicker()
+          .pickImage(source: ImageSource.gallery, imageQuality: 70);
+      if (img == null) return;
+      profileImgPath.value = img.path;
+    } on PlatformException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+  }
 }
