35f4fcf463caf4b4a2ce74c6c680f70e16a32138
aananya <aananyanagpal@gmail.com>
2024-11-27 14:48:12 +0530

    products+firebase

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -202,49 +202,48 @@ class ItemDetails extends StatelessWidget {
                   ),
                   20.heightBox,
 
-
                   //products you may like section
                   productsyoumaylike.text
                       .color(darkFontGrey)
-                      .fontFamily(bold).size(16)
+                      .fontFamily(bold)
+                      .size(16)
                       .make(),
-                   10.heightBox,   
-                   //copied from home screen featured products
-                   SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: List.generate(
-                              6,
-                              (index) => Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Image.asset(imgP1,
-                                          width: 150, fit: BoxFit.cover),
-                                      10.heightBox,
-                                      "Laptop 4GB/64GB"
-                                          .text
-                                          .fontFamily(semibold)
-                                          .color(darkFontGrey)
-                                          .make(),
-                                      10.heightBox,
-                                      "\$600"
-                                          .text
-                                          .color(redColor)
-                                          .fontFamily(bold)
-                                          .size(16)
-                                          .make(),
-                                    ],
-                                  )
-                                      .box
-                                      .white
-                                      .margin(const EdgeInsets.symmetric(
-                                          horizontal: 4))
-                                      .roundedSM
-                                      .padding(const EdgeInsets.all(8))
-                                      .make()),
-                        ),
-                      ),
+                  10.heightBox,
+                  //copied from home screen featured products
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(
+                          6,
+                          (index) => Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(imgP1,
+                                      width: 150, fit: BoxFit.cover),
+                                  10.heightBox,
+                                  "Laptop 4GB/64GB"
+                                      .text
+                                      .fontFamily(semibold)
+                                      .color(darkFontGrey)
+                                      .make(),
+                                  10.heightBox,
+                                  "\$600"
+                                      .text
+                                      .color(redColor)
+                                      .fontFamily(bold)
+                                      .size(16)
+                                      .make(),
+                                ],
+                              )
+                                  .box
+                                  .white
+                                  .margin(
+                                      const EdgeInsets.symmetric(horizontal: 4))
+                                  .roundedSM
+                                  .padding(const EdgeInsets.all(8))
+                                  .make()),
+                    ),
+                  ),
                 ],
               ),
             ),
